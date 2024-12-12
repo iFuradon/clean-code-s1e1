020ccce47c64e678f96bbdd19ae2882f7af4f88d
iFuradon <iFuradon@gmail.com>
2024-12-13 01:10:56 +0400

    refactor(basic-3.1): rename selectors in index.html, style.css, app.js files header: rename "aaa"                              with "header-info" rename "more_inf"                         with "header-info__more" main: rename "centered-main-page-element"       with "main" section: rename "topSectionForAddingElementToList" with "new-item" rename "new-task"                         with "new-item__input" rename "task-row-wrapper"                 with "new-item__wrapper" rename "task"                             with "new-item__task-input" add "add" section add "todo__title" add "todo__list" add "todo__item" add "todo__checkbox" rename "task"                             with "todo__task" rename "incompleteTasks"                  with "todo__tasks" rename "editMode"                         with "todo__item__edit" section add "completed__title" add "completed__list" add "completed__item" add "completed__checkbox" rename "completed-tasks"                  with "completed__tasks" rename "task"                             with "completed__task"

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("new-item__input");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("todo__tasks");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("completed__tasks");//completed-tasks
 
 
 //New task list item
@@ -33,12 +33,12 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='todo__task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="todo__task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
@@ -85,7 +85,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var containsClass=listItem.classList.contains("todo__item__edit");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +99,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("todo__item__edit");
 };
 
 
